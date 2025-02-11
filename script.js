86ffb00540612ad6e0b6f7b7a46c63f17add9f0f
624-urvashi <143706170+624-urvashi@users.noreply.github.com>
2025-02-11 12:07:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 const byteSize = (str) => {
-  // write your code here
-	function bytesize(str){
-		if(typeof str !=== 'string'){
-			throw new Error('Input must be a string');
-		}
-		const blob = new Blob([str]);
-		return blob.size;
-	}
+  if(typeof str !== 'string'){
+    throw new Error('Input must be a string');
+  }
+  const blob = new Blob([str]);
+  return blob.size;
 };
-
 // Do not change the code below
 const str = prompt("Enter some string.");
 alert(byteSize(str));
